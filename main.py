4df425e935ceb5c4ae6eced4660a5f803f3a8862
Your Name <you@example.com>
2021-10-11 16:54:18 -0500

    Add logic

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
 import random
 
-movements = ['piedra', 'papel', 'tijeras']
+movements = ['piedra', 'papel', 'tijera']
+print('\nPiedra, papel o tijera vs una IA\n')
 def game(movements):
     while True:
-        ia = movements[random.randint(len(movements))]
-        user = input('Haz un movimiento: ').lower()
-        
+        user = input('You: ').lower()
+        ia = movements[random.randint(0, 2)]
+        if user == movements[1] and ia == movements[0]:
+            print('IA:', ia)
+            print('¡Ganaste!')
+            break
+        elif user == movements[1] and ia == movements[2]:
+            print('IA:', ia)
+            print('¡Perdiste! :(')
+            break    
+        elif user == movements[1] and ia == movements[1]:
+            print('IA:', ia)
+            print('¡Empate! Jugamos de nuevo.')
+            continue
+        if user == movements[0] and ia == movements[2]:
+            print('IA:', ia)
+            print('¡Ganaste!')
+            break
+        elif user == movements[0] and ia == movements[1]:
+            print('IA:', ia)
+            print('¡Perdiste! :(')
+            break    
+        elif user == movements[0] and ia == movements[0]:
+            print('IA:', ia)
+            print('¡Empate! Jugamos de nuevo.')
+            continue
+        if user == movements[2] and ia == movements[1]:
+            print('IA:', ia)
+            print('¡Ganaste!')
+            break
+        elif user == movements[2] and ia == movements[0]:
+            print('IA:', ia)
+            print('¡Perdiste! :(')
+            break    
+        elif user == movements[2] and ia == movements[2]:
+            print('IA:', ia)
+            print('¡Empate! Jugamos de nuevo.')
+            continue
+
+game(movements)
